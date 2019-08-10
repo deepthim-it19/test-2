9266a5e40c54cac3d8b7e20114e2a07e6c9a8145
deepthim-it19 <deepthim.it19@bitsathy.ac.in>
2019-08-10 11:30:43 +0530

    index2.php

---
+++ index2.php
@@ -0,0 +1,19 @@
+<head>
+<meta charset="utf-8">
+<tittle>calculator</tittle>
+</head>
+<body>
+<form method="post" attribute="post"="disp_form.php">
+<p>First value:<br/>
+<input type="text" id="first" name="first"></p>
+<p>Second value:<br/>
+<input type="text" id="second" name="second"></p>
+<input type="radio" name="group1" id="add" value="add" check="true"><p>+</p><br/>
+<input type="radio" name="group1" id="subtract" value="subtract" check="true"><p>-</p><br/>
+<input type="radio" name="group1"id="times" value="times" check="true"><p>*</p><br/>
+<input type="radio" name="group1" id="division" value="division" check="true"><p>/</p><br>
+<p></p>
+<button type="submit" name="answer" id="answer" value="answer">calculate</button>
+</form>
+</body>
+</html>
